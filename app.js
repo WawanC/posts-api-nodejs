0e7996121043b9dd3d4137667e6f1c9434d3ce7a
wawanc <kurniawancristianto@gmail.com>
2021-09-17 19:25:40 +0700

    Allow CORS

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.use((req, res, next) => {
+  res.status(200).json({
+    message: "Hello",
+  });
+});
+
+app.listen(3000);
